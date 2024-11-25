90757e3c0352078b5f99c4109f895e61915f6d12
Julia_Chu <z3211232001@gmail.com>
2024-11-25 11:39:52 -0800

    finished

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
 $(function() {
   // YOUR CODE HERE
+  //Exercise 1
+  $('#toggleButton').on('click', () => {
+    $('#myParagraph').toggle()
+  })
+
+  //Exercise 2
+  $('#colorButton').on('click', () => {
+    $('#colorDiv').css({
+      background: 'yellow'
+    })
+  })
+
+  //Exercise 3
+  $('#addClassButton').on('click', () => {
+    $('#classDiv').addClass('new-classDiv')
+  })
+
+  $('#removeClassButton').on('click', () => {
+    $('#classDiv').removeClass('new-classDiv')
+  })
+
+  //Exercise 4
+  $('#fadeInButton').on('click', () => {
+    $('#fadeDiv').fadeIn(3000)
+  })
+
+  $('#fadeOutButton').on('click', () => {
+    $('#fadeDiv').fadeOut("slow")
+  })
+
+  //Exercise 5
+  $('#slideUpButton').on('click', () => {
+    $('#slideDiv').slideUp()
+  })
+
+  $('#slideDownButton').on('click', () => {
+    $('#slideDiv').slideDown()
+  })
 })
